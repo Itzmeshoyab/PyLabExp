bf0c5d16230dee77fa950004cfbd3d5902556f31
Shoyab Shaik <shoyabshaik0418@gmail.com>
2026-02-13 13:39:40 +0530

    Updated programs

--- 110554.py
+++ 110554.py
@@ -12,3 +12,26 @@ for i in range(20):
     total = total + numbers[i]
 average = total / 20
 print("Average:", average)
+largest = numbers[0]
+smallest = numbers[0]
+for i in range(1, 20):
+    if numbers[i] > largest:
+        largest = numbers[i]
+    if numbers[i] < smallest:
+        smallest = numbers[i]
+print("Largest value:", largest)
+print("Smallest value:", smallest)
+second_largest = -1
+second_smallest = 101
+for i in range(20):
+    if numbers[i] > second_largest and numbers[i] != largest:
+        second_largest = numbers[i]
+    if numbers[i] < second_smallest and numbers[i] != smallest:
+        second_smallest = numbers[i]
+print("Second Largest value:", second_largest)
+print("Second Smallest value:", second_smallest)
+even_count = 0
+for i in range(20):
+    if numbers[i] % 2 == 0:
+        even_count = even_count + 1
+print("Number of even numbers:", even_count)
